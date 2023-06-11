50c9c7e3134ffc40c914b18d2f7ad25740729e42
deco-01 <ex.k.futatunohoshino@gmail.com>
2023-06-11 17:22:48 +0900

    first commit

---
+++ db/migrate/20230609054217_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.text :content, null: false
+      t.references :prototype, null: false, unique: true
+      t.references :user, null: false, unique: true, dependent: :destroy
+
+      t.timestamps
+    end
+  end
+end
